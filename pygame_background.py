42fa46567c174bca60ded840d5cffbbd1031d8b3
lipton012 <mihasvzpoint@gmail.com>
2023-12-25 22:03:55 +0300

    changed the name of the game

--- pygame_background.py
+++ pygame_background.py
@@ -3,7 +3,7 @@ import pygame
 pygame.init()
 window = pygame.display.set_mode((1000, 700))
 clock = pygame.time.Clock()
-pygame.display.set_caption("John's adventure")
+pygame.display.set_caption("Barbos adventure")
 background = pygame.image.load('background.jpg').convert()
 background = pygame.transform.smoothscale(background, window.get_size())
 
