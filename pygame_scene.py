886892d497ca410973c3db9d910c0caa24ac0efc
lipton012 <mihasvzpoint@gmail.com>
2024-01-24 22:04:00 +0300

    add some changes

--- pygame_scene.py
+++ pygame_scene.py
@@ -1,19 +1,22 @@
 import pygame
+
 objects = []
 frame = 1
 worldx = 960
 worldy = 720
 fps = 40
 ani = 4
+qw = 1
 
-class Button():
-    def __init__(self, x, y, width, height, buttonText='Button', onclickFunction=None, onePress=False):
+
+class Button:
+    def __init__(self, x, y, width, height, button_text='Button', onclick_function=None, one_press=False):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        self.onclickFunction = onclickFunction
-        self.onePress = onePress
+        self.onclickFunction = onclick_function
+        self.onePress = one_press
 
         self.fillColors = {
             'normal': '#ffffff',
@@ -24,7 +27,7 @@ class Button():
         self.buttonSurface = pygame.Surface((self.width, self.height))
         self.buttonRect = pygame.Rect(self.x, self.y, self.width, self.height)
 
-        self.buttonSurf = font.render(buttonText, True, (20, 20, 20))
+        self.buttonSurf = font.render(button_text, True, (20, 20, 20))
 
         self.alreadyPressed = False
 
@@ -32,10 +35,10 @@ class Button():
 
     def process(self):
 
-        mousePos = pygame.mouse.get_pos()
+        mouse_pos = pygame.mouse.get_pos()
 
         self.buttonSurface.fill(self.fillColors['normal'])
-        if self.buttonRect.collidepoint(mousePos):
+        if self.buttonRect.collidepoint(mouse_pos):
             self.buttonSurface.fill(self.fillColors['hover'])
 
             if pygame.mouse.get_pressed(num_buttons=3)[0]:
@@ -70,13 +73,13 @@ class Player(pygame.sprite.Sprite):
         self.frame = 0
         self.images = []
         for i in range(1, 7):
-            if name=="player":
+            if name == "player":
                 img = pygame.image.load(f'zip/main_dog/{i}.png')
             else:
                 img = pygame.image.load(f'bad_hero.png')
-            img = pygame.transform.scale(img,size)
+            img = pygame.transform.scale(img, size)
             img.convert_alpha()  # optimise alpha
-            # img.set_colorkey(ALPHA)  # set alpha
+
             self.images.append(img)
             self.image = self.images[0]
             self.rect = self.image.get_rect()
@@ -92,44 +95,40 @@ class Player(pygame.sprite.Sprite):
         """
         Update sprite position
         """
-        if self.rect.x + self.movex < worldx - 180 and self.rect.x + self.movex > 0:
+        if 0 < self.rect.x + self.movex < worldx - 100:
             self.rect.x = self.rect.x + self.movex
-        # print(self.rect.y + self.movey < worldy - 180 and self.rect.y + self.movey > 0)
 
-        if self.rect.y + self.movey < worldy - 180 and self.rect.y + self.movey > 0:
+        if 0 < self.rect.y + self.movey < worldy - 100:
             self.rect.y = self.rect.y + self.movey
-
-        # self.rect.x = self.rect.x + self.movex
-        # self.rect.y = self.rect.y + self.movey
 
         # moving left
         if self.movex < 0:
             self.frame += 1
-            if self.frame > 3*ani:
+            if self.frame > 3 * ani:
                 self.frame = 0
             self.image = pygame.transform.flip(self.images[self.frame // ani], True, False)
 
         # moving right
         if self.movex > 0:
             self.frame += 1
-            if self.frame > 3*ani:
+            if self.frame > 3 * ani:
                 self.frame = 0
-            self.image = self.images[self.frame//ani]
+            self.image = self.images[self.frame // ani]
+
 
 def next_frame():
     global frame
     frame = 2
 
+
 def show_screen1():
-    customButton = Button(500, 600, 200, 70, 'Start', next_frame)
+    Button(500, 600, 200, 70, 'Start', next_frame)
     pygame.display.set_caption("Barbos adventure")
     background = pygame.image.load('background.jpg').convert()
     background = pygame.transform.smoothscale(background, screen.get_size())
     screen.blit(background, (0, 0))
-    for object in objects:
-        object.process()
-    # background_colour = (255,255,255)
-    # screen.fill(background_colour)
+    for item in objects:
+        item.process()
 
 
 def show_screen2(event):
@@ -154,22 +153,6 @@ def show_screen2(event):
         if event.key == pygame.K_DOWN or event.key == ord('s'):
             player.control(0, -steps)
 
-    if enemy.rect.x == 0: #движение призрака
-        enemy.control(10, 0)
-        print(222)
-
-    if enemy.rect.x == 770 and enemy.rect.y==100:
-        # enemy.control(-1, 0)
-        enemy.control(0, 10)
-        print(111)
-
-
-    if enemy.rect.x == 770 and enemy.rect.y==520:
-        # enemy.control(-1, 0)
-        enemy.control(10, 0)
-        print(333)
-
-
 
 def show_screen3():
     background = pygame.image.load('background2.jpg')
@@ -188,6 +171,32 @@ def show_screen3():
     text_surface2 = font.render(f'Ваш результат: {user_result}', False, (230, 230, 230))
     screen.blit(text_surface2, (400, 100))
 
+
+def show_enemy():
+    global qw
+    if qw == 1:
+        enemy.control(10, 0)  # right
+        qw = qw + 1
+
+    elif enemy.rect.x == 10 and enemy.rect.y == 100:  # движение призрака
+        enemy.control(10, 10)  # right
+    elif enemy.rect.x == 770 and enemy.rect.y == 100:
+        enemy.control(-10, 10)  # down
+
+    elif enemy.rect.x == 770 and enemy.rect.y == 200:
+        enemy.control(-10, -10)  # left
+    elif enemy.rect.x == 20 and enemy.rect.y == 200:
+        enemy.control(10, 10)  # down
+    elif enemy.rect.x == 20 and enemy.rect.y == 300:
+        enemy.control(10, -10)  # right
+    elif enemy.rect.x == 770 and enemy.rect.y == 300:
+        enemy.control(-10, 10)  # down
+    elif enemy.rect.x == 770 and enemy.rect.y == 400:
+        enemy.control(-10, -10)  # left
+    elif enemy.rect.x == 10 and enemy.rect.y == 400:
+        enemy.control(10, -10)  # up
+
+
 pygame.init()
 screen = pygame.display.set_mode([worldx, worldy])
 font = pygame.font.SysFont('Arial', 20)
@@ -195,47 +204,42 @@ clock = pygame.time.Clock()
 backdropbox = screen.get_rect()
 backdrop = pygame.image.load('zip/data/homes_wall1.jpg')
 
-player = Player("player", (100,100))  # spawn player
+player = Player("player", (100, 100))  # spawn player
 player.rect.x = 0  # go to x
 player.rect.y = 0  # go to y
 player_list = pygame.sprite.Group()
 player_list.add(player)
 steps = 10
-enemy = Player("enemy", (100,200))
-enemy.rect.x = 0  # go to x
+enemy = Player("enemy", (100, 200))
+enemy.rect.x = 10  # go to x
 enemy.rect.y = 100  # go to y
-# player_list = pygame.sprite.Group()
 player_list.add(enemy)
-
-
 
 running = True
 while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running=False
-    # show_screen2(event)
-    if frame == 1:
-        show_screen1()
+    for event_game in pygame.event.get():
+        if event_game.type == pygame.QUIT:
+            running = False
 
-    elif frame==2:
-        show_screen2(event)
+        if frame == 1:
+            show_screen1()
 
-    else:
-        show_screen3()
+        elif frame == 2:
+            show_screen2(event_game)
 
-  if frame==2:
-     screen.blit(backdrop, backdropbox)
-     player.update()
-     enemy.update()
-     player_list.draw(screen)
+        else:
+            show_screen3()
+
+    if frame == 2:
+        show_enemy()
+        screen.blit(backdrop, backdropbox)
+        player.update()
+        enemy.update()
+        player_list.draw(screen)
 
 
-  # screen.blit(backdrop, backdropbox)
-  # player.update()
-  # player_list.draw(screen)
-  pygame.display.flip()
-  clock.tick(fps)
+    pygame.display.flip()
+    clock.tick(fps)
 
 pygame.quit()
 exit()
